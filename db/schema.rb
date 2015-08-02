a21f69de86ca9eaf9cdeb6a754221b077ceccd2f
Julian Sansat <kerarchie@yahoo.com.ar>
2015-08-01 21:00:29 -0300

    character modeling

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150725221638) do
+ActiveRecord::Schema.define(version: 20150801235942) do
+
+  create_table "characters", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "xp"
+    t.integer  "gold"
+    t.string   "img"
+    t.integer  "User_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "characters", ["User_id"], name: "index_characters_on_User_id"
 
   create_table "questions", force: :cascade do |t|
     t.string   "lesson"
