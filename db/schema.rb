717d65e9f31b02caa318366ebc53fb526776edc4
Julian Sansat <kerarchie@yahoo.com.ar>
2015-08-07 22:40:31 -0300

    The problem was in the migrations, it was causing conflicts with de local database.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150808013052) do
+ActiveRecord::Schema.define(version: 20150808013626) do
+
+  create_table "characters", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "xp"
+    t.integer  "gold"
+    t.string   "hair"
+    t.string   "skin"
+    t.string   "shirt"
+    t.string   "pants"
+    t.integer  "User_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "characters", ["User_id"], name: "index_characters_on_User_id"
 
   create_table "lessons", force: :cascade do |t|
     t.string   "url"
