aeaa25244fbbfe646515deb027867d6382dc7edd
Jefferson Augusto <jeffersonxplode@gmail.com>
2015-08-09 18:57:59 -0300

    Pronto a parte do Admin pronta, deixei no registro do novo usuario uma combobox para testes.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,13 +5,13 @@ class RegistrationsController < Devise::RegistrationsController
 
   def sign_up_params
 
-    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    params.require(:user).permit(:name,:email,:admin,:password,:password_confirmation)
 
   end
 
   def account_update_params
 
-    params.require(:user).permit(:name,:email,:password,:password_confirmation,:current_password)
+    params.require(:user).permit(:name,:email,:admin,:password,:password_confirmation,:current_password)
 
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150808013626) do
+ActiveRecord::Schema.define(version: 20150809212350) do
 
   create_table "characters", force: :cascade do |t|
     t.string   "name"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20150808013626) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
