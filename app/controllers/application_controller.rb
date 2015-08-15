302a4107cbbce4526eb6ad0d944a884e0e6b41b7
Julian Sansat <kerarchie@yahoo.com.ar>
2015-08-15 16:43:41 -0300

    implemented the bootstrap layout made by WendellHolden into the actual application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :getCharacter
+
+def getCharacter
+	if user_signed_in?
+		@characters = Character.where(User_id: current_user)
+		if @characters.size == 1
+			@character = @characters.first
+		end
+	end  
+end
+
 end
