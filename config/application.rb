fc43f5a272fda1b63d319da0e0a99906b2caadab
Julian Sansat <kerarchie@yahoo.com.ar>
2015-08-25 16:57:28 -0300

    Added some bootstrap layouts

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Reino
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
     ActionMailer::Base.smtp_settings = {
 
         :address     => 'smtp.gmail.com',
